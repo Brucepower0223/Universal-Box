bf0d3ff7163d1306db6e775310a0b2fafdc1a8b6
fangjin <313011249@qq.com>
2017-12-17 23:08:45 +0800

    完善分布式锁的设计

--- src/main/java/com/common/zookeeper/distributedLock/zkversion1/BaseDistributedLock.java
+++ src/main/java/com/common/zookeeper/distributedLock/zkversion1/BaseDistributedLock.java
@@ -5,6 +5,7 @@ import org.apache.zookeeper.data.Stat;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,20 +21,23 @@ public class BaseDistributedLock implements Watcher, DistrubutedLock {
     private String waitLock;
     private String currentLock;
 
-    private int sessionTimeout = 100000000;
+    private int sessionTimeout = 5000;
     private Object mutex = new Object();
 
     public static volatile boolean isDisconnect = false;
+    private CountDownLatch connectedSignal = new CountDownLatch(1);
+    private static int num;
 
 
     public BaseDistributedLock() {
     }
 
 
-    public BaseDistributedLock(String config, String lockName) {
+    public BaseDistributedLock(String config) {
         try {
             client = new ZooKeeper(config, sessionTimeout, this);
-            Thread.sleep(1000);
+            //建立连接后，才能进行下一步操作
+            connectedSignal.await();
             Stat exists = client.exists(LOCK_ROOT, false);
             if (exists == null) {
                 client.create(LOCK_ROOT, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
@@ -45,13 +49,18 @@ public class BaseDistributedLock implements Watcher, DistrubutedLock {
 
     @Override
     public void process(WatchedEvent watchedEvent) {
-        System.out.println("received the type = " + watchedEvent.getType());
         if (watchedEvent.getType() == Event.EventType.NodeDeleted) {
             isDisconnect = true;
-            System.out.println("isDisconnected = true");
             synchronized (mutex) {
                 mutex.notify();
             }
+        }
+        //建立连接后，才能进行create exist等操作
+        if (watchedEvent.getState() == Event.KeeperState.SyncConnected && watchedEvent.getType() != Event.EventType.NodeDeleted) {
+            num++;
+            System.out.println("num = " + num);
+            connectedSignal.countDown();
+            return;
         }
     }
 
@@ -66,15 +75,18 @@ public class BaseDistributedLock implements Watcher, DistrubutedLock {
         currentLock = client.create(LOCK_ROOT + "/" + LOCK_NAME,
                 new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
         currentLock = currentLock.substring(currentLock.lastIndexOf("/") + 1);
-        System.out.println(Thread.currentThread().getName() + currentLock + " has been created!!!");
         List<String> children = client.getChildren(LOCK_ROOT, false);
-        Collections.sort(children);
-        if (currentLock.equals(children.get(0))) {
-            System.out.println(Thread.currentThread().getName() + " has the lock named " + currentLock);
+        if (children.size() != 1) {
+            Collections.sort(children);
+            if (currentLock.equals(children.get(0))) {
+                System.out.println(Thread.currentThread().getName() + " has the lock named " + currentLock);
+            } else {
+                //若不是最小的节点
+                waitLock = children.get(children.indexOf(currentLock) - 1);
+                waitForLock();
+            }
         } else {
-            //若不是最小的节点
-            waitLock = children.get(children.indexOf(currentLock) - 1);
-            waitForLock();
+            System.out.println("当前只有一把锁");
         }
     }
 
@@ -93,7 +105,6 @@ public class BaseDistributedLock implements Watcher, DistrubutedLock {
                     List<String> children = client.getChildren(LOCK_ROOT, false);
                     Collections.sort(children);
                     if (currentLock.equals(children.get(0))) {
-                        System.out.println(Thread.currentThread().getName() + " has the lock named " + currentLock);
                         System.out.println(Thread.currentThread().getName() + " 等到了锁");
                     }
                     isDisconnect = false;
@@ -113,6 +124,4 @@ public class BaseDistributedLock implements Watcher, DistrubutedLock {
         //删除节点
         client.delete(LOCK_ROOT + "/" + currentLock, -1);
     }
-
-
 }
--- src/main/java/com/common/zookeeper/distributedLock/zkversion1/Test.java
+++ src/main/java/com/common/zookeeper/distributedLock/zkversion1/Test.java
@@ -1,37 +1,37 @@
 package com.common.zookeeper.distributedLock.zkversion1;
 
 
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
 /**
  * @author Bruce
  * @date 2017/11/23
  */
 public class Test {
     public static void main(String[] args) throws Exception {
-
-
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 500; i++) {
             Thread thread = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
                         DistrubutedLock lock = null;
-                        lock = new BaseDistributedLock("192.168.41.106:2181", "test1");
+                        lock = new BaseDistributedLock("192.168.41.106:2181");
                         System.out.println(Thread.currentThread().getName() + "正在运行");
-                        Thread.sleep(1000);
                         lock.acquire();
                         System.out.println(Thread.currentThread().getName() + "处理自己的业务");
-                        Thread.sleep(10000);
                         lock.release();
+//                        ZooKeeper zooKeeper = new ZooKeeper("192.168.41.106:2181", 3000, new Watcher() {
+//                            @Override
+//                            public void process(WatchedEvent watchedEvent) {
+//                                num++;
+//                                System.out.println("num = " + num);
+//                            }
+//                        });
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-//                    ZooKeeper client;
-//                    try {
-//                        client = new ZooKeeper("192.168.41.106:2181", 6000, null);
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-
                 }
             });
             thread.start();
