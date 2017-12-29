6979c6d549466087fb158c25dccd1f3d005d0fb4
Bruce <313011249@qq.com>
2017-12-29 11:01:53 +0800

    提交爬虫类

---
+++ src/main/java/com/whut/crawler/AppInfoCrawler.java
@@ -0,0 +1,323 @@
+package com.whut.crawler;
+
+import com.common.utils.file.FileUtils;
+import com.common.utils.StringUtils;
+import com.common.utils.WebClientUtils;
+import com.gargoylesoftware.htmlunit.HttpMethod;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
+import com.whut.entity.KuBao;
+import com.whut.entity.TaskEntity;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 从酷安上抓取App信息
+ * <p>
+ * Created by Bruce on 2017/9/20.
+ */
+public class AppInfoCrawler {
+
+    static {
+        webClient = WebClientUtils.getWebClient();
+    }
+
+    /**
+     * 任务队列
+     */
+    public static BlockingQueue<TaskEntity> taskQuene = new LinkedBlockingQueue<TaskEntity>();
+    public static ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+    public static String domain = "https://www.coolapk.com";
+
+    public static WebClient webClient;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    crawlerData();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        try {
+            for (int i = 0; i < 10; i++) {
+                Thread t = new Thread(new Runnable() {
+                    public void run() {
+                        while (true) {
+                            TaskEntity task = getTaskFromQuene();
+                            if (task != null) {
+                                crawlerData(task);
+                            }
+                        }
+                    }
+                });
+                threadPool.execute(t);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //parseData();
+    }
+
+    /**
+     * 通过拿到的任务抓取结果
+     *
+     * @param task
+     */
+    public static void crawlerData(TaskEntity task) {
+        try {
+            System.out.println(Thread.currentThread().getName() + ":抓取" + task.getAppName() + "---->" + task.getUrl());
+            System.out.println();
+            WebRequest webRequest1 = new WebRequest(new URL(task.getUrl()), HttpMethod.GET);
+            Page appPage = WebClientUtils.getWebRequestPage(webRequest1, webClient);
+            FileUtils.saveToFile("g:/apps01/" + task.getFirst() + "/" + task.getSecond() + "/" + task.getAppName() + ".html",
+                    appPage.getWebResponse().getContentAsString());
+        } catch (Exception e) {
+            System.out.println(task.getAppName() + "抓取出错");
+        }
+    }
+
+    /**
+     * 从任务队列获取任务
+     *
+     * @return
+     */
+    public static TaskEntity getTaskFromQuene() {
+        try {
+            return taskQuene.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 解析data
+     */
+    public static void parseData() {
+        File file = new File("g:/apps01");
+        List<KuBao> kubaos = new ArrayList<KuBao>();
+        for (File f : file.listFiles()) {
+            for (File f2 : f.listFiles()) {
+                for (File f1 : f2.listFiles()) {
+                    try {
+                        if (!f1.isDirectory()) {
+                            KuBao kubao = new KuBao();
+                            Document document = Jsoup.parse(FileUtils.readFileContent(f1.getAbsolutePath()));
+                            String domain = "应用分类";
+                            String first = f1.getParentFile().getParentFile().getName();
+                            String second = f1.getParentFile().getName();
+                            String appName = document.select("p[class=detail_app_title]").text();
+                            String introduction = document.select("div[class=apk_left_title_info]").text();
+                            kubao.setDomain(domain);
+                            kubao.setFirst(first);
+                            kubao.setSecond(second);
+                            kubao.setAppName(appName);
+                            kubao.setIntroduction(introduction);
+                            kubaos.add(kubao);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println("解析" + f.getAbsolutePath() + "出错");
+                    }
+                }
+            }
+        }
+
+        saveToExcel(kubaos);
+    }
+
+
+    public static String s[] = new String[]{"应用市场名称", "一级分类", "二级分类", "app名称", "app介绍文字详情"};
+    /**
+     * 创建excel表头
+     *
+     * @param sheet
+     */
+    private static void createRowHeader(XSSFSheet sheet) {
+        XSSFRow row = sheet.createRow(0);
+        for (int i = 0; i < s.length; i++) {
+            XSSFCell cell = row.createCell(i);
+            cell.setCellValue(s[i]);
+        }
+    }
+
+    private static void saveToExcel(List<KuBao> lists) {
+        String outputPath = "e:/output1121.xlsx";
+        FileOutputStream out = null;
+        try {
+            XSSFWorkbook workBook = new XSSFWorkbook();
+            XSSFSheet sheet = workBook.createSheet("111111");
+            createRowHeader(sheet);
+            for (KuBao kubao : lists) {
+                if (kubao != null) {
+                    XSSFRow row = sheet.createRow(sheet.getLastRowNum() + 1);
+                    XSSFCell cell = row.createCell(0);
+                    cell.setCellValue(kubao.getDomain());
+
+                    cell = row.createCell(1);
+                    cell.setCellValue(kubao.getFirst());
+
+                    cell = row.createCell(2);
+                    cell.setCellValue(kubao.getSecond());
+
+
+                    cell = row.createCell(3);
+                    cell.setCellValue(kubao.getAppName());
+
+                    cell = row.createCell(4);
+                    cell.setCellValue(kubao.getIntroduction());
+                }
+            }
+
+            out = new FileOutputStream(outputPath);
+            workBook.write(out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * 抓取App页面的信息
+     */
+    public static void crawlerData() throws Exception {
+        String url = "https://www.coolapk.com/apk";
+        webClient.getOptions().setJavaScriptEnabled(false);
+        WebRequest request = new WebRequest(new URL(url), HttpMethod.GET);
+        Page page = WebClientUtils.getWebRequestPage(request, webClient);
+
+
+        Document document = Jsoup.parse(page.getWebResponse().getContentAsString());
+        Elements divs = document.select("div[class=type_list]");
+        for (Element div : divs) {
+            String firstCategory = div.select("p[class=type_title]").text();
+            Elements hrefs = div.select("p[class=type_tag]").select("a");
+            for (Element a : hrefs) {
+                String href = domain + a.attr("href");
+                String secondCategory = a.text();
+                crawlerDetail(firstCategory, secondCategory, href);
+            }
+        }
+    }
+
+    /**
+     * 抓取分类详情页面
+     *
+     * @param firstCategory
+     * @param href
+     */
+    private static void crawlerDetail(String firstCategory, String secondCategory, String href) {
+        try {
+            WebRequest webRequest = new WebRequest(new URL(href), HttpMethod.GET);   //抓取分页的信息
+            Page details = WebClientUtils.getWebRequestPage(webRequest, webClient);
+            if (details != null) {
+                int lastPage = getLastPageNum(details);
+                saveApp(firstCategory, secondCategory, details);
+                if (lastPage > 1) {
+                    int i = 2;
+                    while (i <= lastPage) {
+                        String url = href + "?p=" + i;
+                        webRequest = new WebRequest(new URL(url), HttpMethod.GET);
+                        details = WebClientUtils.getWebRequestPage(webRequest, webClient);
+                        saveApp(firstCategory, secondCategory, details);
+                        i++;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(firstCategory + "---" + secondCategory + "抓取出错");
+        }
+    }
+
+
+    /**
+     * 保存每一个App的信息
+     *
+     * @param firstCategory
+     * @param secondCategory
+     * @param details
+     */
+    private static void saveApp(String firstCategory, String secondCategory, Page details) {
+        String url = "";
+        String context = details.getWebResponse().getContentAsString();
+        Document document = Jsoup.parse(context);
+        Elements detailHref = document.select("div[class=app_list_left]").select("a");
+        for (Element a : detailHref) {
+            if (a.select("div[class=alllist_app_side]").size() != 0) {
+                String appName = "";
+                try {
+                    url = domain + a.attr("href");
+                    appName = a.select("p[class=list_app_title]").text();
+                    if (appName.contains(":")) {
+                        appName = StringUtils.replaceKeyTab(appName).replace(":", "");
+                    }
+                    TaskEntity task = new TaskEntity();
+                    task.setFirst(firstCategory);
+                    task.setSecond(secondCategory);
+                    task.setUrl(url);
+                    task.setAppName(appName);
+                    taskQuene.put(task);
+                } catch (Exception e) {
+                    System.out.println(appName + "抓取出错");
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取详情页面最后一页
+     *
+     * @param details
+     * @return
+     */
+    private static int getLastPageNum(Page details) {
+        int pageNum = 1;
+        try {
+            Document doc = Jsoup.parse(details.getWebResponse().getContentAsString());
+            Elements lis = doc.select("li");
+            for (Element e : lis) {
+                if (e.text().contains("尾页")) {
+                    String url = e.select("a").attr("href");
+                    String numStr = url.substring(url.indexOf("p=") + 2, url.length());
+                    pageNum = Integer.parseInt(numStr);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("获取页码错误");
+            e.printStackTrace();
+        }
+        return pageNum;
+    }
+
+}
---
+++ src/main/java/com/whut/crawler/WebsiteCrawler.java
@@ -0,0 +1,422 @@
+package com.whut.crawler;
+
+import com.alibaba.fastjson.JSONObject;
+import com.common.utils.StringUtils;
+import com.common.utils.WebClientUtils;
+import com.common.utils.file.FileUtils;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * 抓取网站排行的信息
+ *
+ * @author Bruce
+ * @date 2017/12/19
+ */
+public class WebsiteCrawler {
+
+    public String categories[] = {"购物网站", "交通旅游", "休闲娱乐", "教育文化", "生活服务", "医疗健康"};
+    WebClient webClient = null;
+    public String prefix = "http://top.chinaz.com/hangye/";
+    public String indexUrl = "http://top.chinaz.com/hangye/index.html";
+    private static List<Task> tasks = new ArrayList<Task>();
+
+
+    public static void main(String[] args) throws Exception {
+
+        WebsiteCrawler websiteCrawler = new WebsiteCrawler();
+        websiteCrawler.getTask();
+        System.out.println("所有分类起始网站抓取完毕");
+        for (Task task : tasks) {
+            System.out.println("【" + task.getFirst() + "---" + task.getSecond() + "】开始抓取");
+            new Thread(new CrawlUrlThread(task)).start();
+        }
+    }
+
+    public void getTask() throws Exception {
+        webClient = WebClientUtils.getWebClient();
+        webClient.getOptions().setJavaScriptEnabled(false);
+        Page page = webClient.getPage(indexUrl);
+        if (page != null) {
+            String result = page.getWebResponse().getContentAsString();
+            Document docment = Jsoup.parse(result);
+            Elements elements = docment.select("div[class=HeadFilter clearfix]").select("a");
+            for (Element a : elements) {
+                String text = a.text();
+                if (isContains(text)) {
+                    String href = prefix + a.attr("href");
+                    tasks.addAll(getSecondTask(href, text));
+                }
+            }
+        }
+        System.out.println(JSONObject.toJSONString(tasks, true));
+    }
+
+    /**
+     * 获取二级页面连接
+     *
+     * @param href
+     * @return
+     */
+    private List<Task> getSecondTask(String href, String first) throws Exception {
+        System.out.println("开始抓取【" + first + "】分类");
+        Page page = webClient.getPage(href);
+        if (page != null) {
+            List<Task> tasks = new ArrayList<Task>();
+            Document document = Jsoup.parse(page.getWebResponse().getContentAsString());
+            Elements as = document.select("div[class=HeadFilter clearfix]").select("a");
+            for (Element element : as) {
+                String second = element.text();
+                String url = element.attr("href");
+                Task task = new Task(prefix + url, first, second);
+                tasks.add(task);
+            }
+            System.out.println("抓取【" + first + "】分类完毕");
+
+            return tasks;
+        }
+        return null;
+    }
+
+
+    private boolean isContains(String src) {
+        for (String category : categories) {
+            if (category.equals(src)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
+
+
+class CrawlUrlThread implements Runnable {
+    public String prefix = "http://top.chinaz.com/hangye/";
+    Task task;
+    WebClient webClient;
+    List<String> urls = new ArrayList<String>();
+
+    CrawlUrlThread() {
+
+    }
+
+    CrawlUrlThread(Task task) {
+        webClient = WebClientUtils.getWebClient();
+        webClient.getOptions().setJavaScriptEnabled(false);
+        webClient.getOptions().setTimeout(10000);
+        this.task = task;
+    }
+
+    @Override
+    public void run() {
+        String url = "";
+        try {
+            url = task.getUrl();
+            Page page = webClient.getPage(url);
+            if (page != null) {
+                String context = page.getWebResponse().getContentAsString();
+                int pageNum = getPageNum(context);
+                System.out.println(Thread.currentThread().getName() + task.getFirst() + task.getSecond() + "总共" + pageNum + "页");
+                for (int i = 1; i <= pageNum; i++) {
+                    String crawlUrl = getUrl(i, url);
+                    System.out.println(Thread.currentThread().getName() + "开始抓取" + task.getSecond() + "第" + i + "页信息");
+                    try {
+                        Page page1 = webClient.getPage(crawlUrl);
+                        FileUtils.saveToFile("G:/allurls/" + task.getFirst() + "/" + task.getSecond() + "/" + i + ".html", page1.getWebResponse().getContentAsString());
+                        System.out.println(Thread.currentThread().getName() + "----------------抓取" + task.getSecond() + "第" + i + "页信息完毕-------------------");
+                    } catch (Exception e) {
+                        System.out.println("********************抓取" + url + "出错");
+                        e.printStackTrace();
+                        continue;
+                    }
+
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("【" + task.getFirst() + "-" + task.getSecond() + "】" + url + "抓取失败");
+        }
+    }
+
+
+//    public static void main(String[] args) {
+//        CrawlUrlThread crawlUrlThread = new CrawlUrlThread();
+//        System.out.println(crawlUrlThread.getUrl(1, "http://top.chinaz.com/hangye/index_yule_youxi.html"));
+//
+//    }
+
+
+    private String getUrl(int pageNum, String url) {
+        String ret = "";
+        if (pageNum == 1) {
+            ret = url;
+        } else {
+            ret = url.replace(".html", "_" + pageNum + ".html");
+        }
+        return ret;
+    }
+
+    private int getPageNum(String context) {
+        Document document = Jsoup.parse(context);
+        Elements as = document.select("div[class=ListPageWrap]").select("a");
+        return Integer.parseInt(as.get(as.size() - 2).text());
+    }
+
+
+}
+
+class Parser {
+
+    public String s[] = new String[]{"一级分类", "二级分类", "网站名称", "网址", "Alexa周排名"};
+
+
+    public static void main(String[] args) {
+        Parser parser = new Parser();
+        parser.parserData();
+    }
+
+    /**
+     * 创建excel表头
+     *
+     * @param sheet
+     */
+    private void createRowHeader(XSSFSheet sheet) {
+        XSSFRow row = sheet.createRow(0);
+        for (int i = 0; i < s.length; i++) {
+            XSSFCell cell = row.createCell(i);
+            cell.setCellValue(s[i]);
+        }
+    }
+
+
+    private void saveToExcel(List<WebsiteEntity> lists, String first) {
+        String outputPath = "g:/" + first + ".xlsx";
+        FileOutputStream out = null;
+        try {
+            XSSFWorkbook workBook = new XSSFWorkbook();
+            XSSFSheet sheet = workBook.createSheet("111111");
+            createRowHeader(sheet);
+            for (WebsiteEntity website : lists) {
+                if (website != null) {
+                    XSSFRow row = sheet.createRow(sheet.getLastRowNum() + 1);
+                    XSSFCell cell = row.createCell(0);
+                    cell.setCellValue(website.getFirst());
+
+                    cell = row.createCell(1);
+                    cell.setCellValue(website.getSecond());
+
+                    cell = row.createCell(2);
+                    cell.setCellValue(website.getName());
+
+                    cell = row.createCell(3);
+                    cell.setCellValue(website.getWebsite());
+
+
+                    cell = row.createCell(4);
+                    if (website.getRank() != 0) {
+                        cell.setCellValue(website.getRank());
+                    } else {
+                        cell.setCellValue("---");
+                    }
+
+                }
+            }
+
+            out = new FileOutputStream(outputPath);
+            workBook.write(out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    Map<Integer, String> cache = new HashMap<Integer, String>();
+
+    public void parserData() {
+        String prefix = "www.";
+        String savePath = "G:/allurls";
+        File file = new File(savePath);
+        System.out.println(file.listFiles().length);
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            for (File firstFile : files) {
+                List<WebsiteEntity> entities = new ArrayList<WebsiteEntity>();
+                String first = firstFile.getName();
+                File[] secondFiles = firstFile.listFiles();
+                for (File secondFile : secondFiles) {
+                    String second = secondFile.getName();
+                    for (File htmlFile : secondFile.listFiles()) {
+                        String context = FileUtils.readFileContent(htmlFile.getAbsolutePath());
+                        Document document = Jsoup.parse(context);
+                        Elements lis = document.select("ul[class=listCentent]").select("li");
+                        for (Element li : lis) {
+                            try {
+                                WebsiteEntity entity = new WebsiteEntity();
+                                String url = prefix + li.select("span[class=col-gray]").text();
+                                String rank = li.select("p[class=RtCData]").get(0).select("a").text();
+                                String name = li.select("h3[class=rightTxtHead]").select("a").attr("title");
+                                entity.setWebsite(url);
+                                int rankInt = 0;
+                                try {
+                                    rankInt = Integer.parseInt(rank);
+                                } catch (Exception e) {
+                                    rankInt = 0;
+                                }
+                                entity.setRank(rankInt);
+                                entity.setFirst(first);
+                                entity.setSecond(second);
+                                entity.setName(name);
+                                String cacheUrl = cache.get(rankInt);
+                                if (!StringUtils.isBlankOrNull(cacheUrl)) {
+                                    if (url.length() < cacheUrl.length()) {
+                                        cache.put(new Integer(rankInt), url);
+                                        entities.add(entity);
+                                    } else {
+                                        continue;
+                                    }
+                                } else {
+                                    cache.put(new Integer(rankInt), url);
+                                    entities.add(entity);
+                                }
+                                if (rankInt == 0) {
+                                    entities.add(entity);
+                                }
+                            } catch (Exception e) {
+                                System.out.println("解析" + htmlFile.getAbsolutePath() + "出错");
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                    }
+                }
+
+                Collections.sort(entities, new Comparator<WebsiteEntity>() {
+                    @Override
+                    public int compare(WebsiteEntity o1, WebsiteEntity o2) {
+                        if (o1.getRank() > o2.getRank()) {
+                            return 1;
+                        }
+                        if (o1.getRank() < o2.getRank()) {
+                            return -1;
+                        }
+                        return 0;
+                    }
+                });
+
+                saveToExcel(entities, first);
+
+            }
+        }
+    }
+
+}
+
+
+class Task {
+    private String url;
+    private String first;
+    private String second;
+
+    Task() {
+    }
+
+    Task(String url, String first, String second) {
+        this.url = url;
+        this.first = first;
+        this.second = second;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+}
+
+class WebsiteEntity {
+    String first;
+    String second;
+    String website;
+    int rank;
+    String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
